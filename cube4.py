e31dbb075a23d8893d1345f7f1c15c987b40fda8
NKuladeep <139630947+NKuladeep@users.noreply.github.com>
2023-07-16 12:25:12 +0530

    Add files via upload

---
+++ cube4.py
@@ -0,0 +1,31 @@
+import random
+
+def roll_dice():
+  
+    dice1 = random.randint(4, 6)
+    dice2 = random.randint(4, 6)
+    
+    # Check if both dice show one
+    if dice1 == 4 and dice2 == 4:
+        return True
+    else:
+        return False
+
+
+count = 0
+
+while True:
+    user_input = input("Enter 'r' to roll the dice or 'q' to quit: ")
+    
+    if user_input == 'r':
+        if roll_dice():
+            count += 1
+            print("Two ones occurred!")
+        else:
+            print("No two ones.")
+    elif user_input == 'q':
+        break
+    else:
+        print("Invalid input. Try again.")
+
+print(f"The number of times two ones occurred: {count}")
